6e051d3e05888ac788afa963426c62c5733c9c2a
amlandy <amlandy@umich.edu>
2019-08-15 19:21:57 -0400

    UI is not good but it is usable

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import './App.css';
-import ImageUploader from 'react-images-upload';
-// import Button from '@material-ui/core/Button';
-// import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
 // import InputBase from '@material-ui/core/InputBase';
 // import SearchIcon from '@material-ui/icons/Search';
 // import { ToastContainer, toast } from 'react-toastify';
@@ -16,70 +15,113 @@ class App extends React.Component{
   constructor(props) {
         super(props);
           this.state = {
-            mageURL: '',
-            pictures: []
+            imageURL: '',
+            picture: {},
+            uploadInput: null,
+            results: null
           };
-         this.onDrop = this.onDrop.bind(this);
+         // this.onDrop = this.onDrop.bind(this);
+         this.change = this.change.bind(this);
          this.handleUploadImage = this.handleUploadImage.bind(this);
     }
 
-  // When a picture is uploaded
-  onDrop(picture) {
-      // Set the picture
-      this.setState({
-          pictures: this.state.pictures.concat(picture),
-      });
+    change(e){
+      console.log("Change", e);
+      // var file = this.refs.file.files[0];
+      var reader = new FileReader();
+      this.render();
+      // var url = reader.readAsDataURL(file);
+    }
 
-      console.log(picture);
-  }
+  // // When a picture is uploaded
+  // onDrop(new_picture) {
+  //     // Set the picture
+  //     this.setState({
+  //         picture: new_picture,
+  //     });
+  //
+  //     console.log(new_picture);
+
+  // }
 
   handleUploadImage(e) {
     e.preventDefault();
 
     const data = new FormData();
     data.append('file', this.uploadInput.files[0]);
-    data.append('filename', this.fileName.value);
+    // data.append('filename', this.fileName.value);
 
-    fetch('http://localhost:8000/upload', {
-      method: 'POST',
-      body: data,
+    fetch('http://0.0.0.0:5000/upload', {
+      method: 'PUT',
+      body: data
     }).then((response) => {
-      response.json().then((body) => {
-        this.setState({ imageURL: `http://localhost:8000/${body.file}` });
+      response.json().then((data) => {
+        this.setState({
+          results: data
+        });
       });
+      // response.json().then((body) => {
+      //   this.setState({ imageURL: `http://localhost:8000/${body.file}` });
+      // });
     });
   }
 
 
   render(){
-    console.log("Render app");
+    console.log("Render app", this.state.results);
+    let results;
+    if (this.state.results){
+      results = <div>
+          <h3>Prediction...</h3>
+          {this.state.results[0][0]}
+          </div>
+    }
+    else{
+      results = <p></p>
+    }
+
     return(
       <div>
         <h1>Traffic Sign Recognition</h1>
-        <p> Currently Supports: Stop Sign, Rail Road Crossing, Yield, One Way, Handicapped Parking, Pedestrian Crossing, Wrong Way</p>
-        <ImageUploader
-            withIcon={true}
-            buttonText='Choose images'
-            onChange={this.onDrop}
-            imgExtension={['.jpg', '.gif', '.png', '.gif']}
-            maxFileSize={5242880}
-        />
-        <form onSubmit={this.handleUploadImage}>
-                <div>
-                  <input ref={(ref) => { this.uploadInput = ref; }} type="file" />
-                </div>
-                <div>
-                  <input ref={(ref) => { this.fileName = ref; }} type="text" placeholder="Enter the desired name of file" />
-                </div>
-                <br />
-                <div>
-                  <button>Upload</button>
-                </div>
-                <img src={this.state.imageURL} alt="img" />
-              </form>
+        <h3> Currently Supports... </h3>
+        <div>
+            <img src={ require('./Images/stop.png') } alt='Stop' width='50' height='50'/>
+            <img src={ require('./Images/oneway.png') } alt='One Way'width='50' height='50'/>
+            <img src={ require('./Images/park.png') } alt='Handicap Parking'width='50' height='50'/>
+            <img src={ require('./Images/do_not_enter.png') } alt='Do Not Enter'width='50' height='50'/>
+            <img src={ require('./Images/pedestrian_crossing.png') } alt='Pedestrian'width='50' height='50'/>
+            <img src={ require('./Images/RR2.png') } alt='Rail Road'width='50' height='50'/>
+            <img src={ require('./Images/rw.png') } alt='Road Work'width='50' height='50'/>
+            <img src={ require('./Images/yield.png') } alt='Yield'width='50' height='50'/>
         </div>
+
+        <br />
+        <br />
+
+        <div>
+          <h3>Upload an image to classify it</h3>
+          <input onChange={this.change} ref={(ref) => { this.uploadInput = ref; }} type="file" />
+          <Button onClick={this.handleUploadImage} variant="contained" size="medium" >Classify</Button>
+        </div>
+
+        <br />
+        <br />
+
+        <div>
+          {results}
+        </div>
+
+      </div>
     )
   }
 }
+// <img src={ require('stop_sign_copy.png') } />
+//           <img src='./stop_sign_copy.png' alt="img" />
 
+
+// <img src={this.state.imageURL} alt="img" />
+// App.configure(() => {
+//   var cors = require('cors');
+//   App.use(cors());
+// })
 export default App;
